c3c9de9908f418c22d125575c7b267e38d7a8903
Nihad Hassan Hassan <20912017200144@fci.zu.edu.eg>
2022-04-08 00:26:12 +0200

    project

---
+++ src/com/invoicesales/InvoiceLine.java
@@ -0,0 +1,80 @@
+package com.invoicesales;
+
+import com.invoicesales.InvoiceHeader;
+
+
+public class InvoiceLine 
+{
+    String itemName;
+    float  itemPrice;
+    int    itemCount;
+    float  itemTotal;
+    InvoiceHeader mainInvoice; //aggregation
+
+    public InvoiceLine(String itemName, float itemPrice, int itemCount, InvoiceHeader mainInvoice) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemCount = itemCount;
+        this.mainInvoice = mainInvoice;
+    }
+
+    public InvoiceLine(String invoiceitem, float itemprice, int itemcount, InvoiceHeader temp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public InvoiceLine(String invoiceitem, float itemprice, int itemcount, InvoiceHeader temp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public InvoiceLine(String invoiceitem, float itemprice, int itemcount, InvoiceHeader temp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public InvoiceLine(String invoiceitem, float itemprice, int itemcount, InvoiceHeader temp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public InvoiceLine(String invoiceitem, float itemprice, int itemcount, InvoiceHeader temp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public float getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(float itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public float getItemTotal() {
+        return itemTotal;
+    }
+
+    public void setItemTotal(float itemTotal) {
+        this.itemTotal = itemTotal;
+    }
+
+    public InvoiceHeader getMainInvoice() {
+        return mainInvoice;
+    }
+
+    public void setMainInvoice(InvoiceHeader mainInvoice) {
+        this.mainInvoice = mainInvoice;
+    }   
+}
